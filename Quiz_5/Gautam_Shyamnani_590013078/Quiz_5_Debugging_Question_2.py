bb9e9edf9ec94029422ace28ebf693854ea7f5a6
gautamshyamnani <gautamshyamnani@gmail.com>
2025-07-20 20:41:48 +0530

    Create Quiz_5_Debugging_Question_2.py

---
+++ Quiz_5/Gautam_Shyamnani_590013078/Quiz_5_Debugging_Question_2.py
@@ -0,0 +1,19 @@
+def is_power_of_two(n):
+    # Rule out non-positive numbers
+    if n <= 0:
+        return False
+
+    # Check if n has only one bit set
+    return (n & (n - 1)) == 0
+
+def check_numbers(arr):
+    print("Checking which numbers are powers of two:")
+    for num in arr:
+        if is_power_of_two(num):
+            print(f"{num}: YES")
+        else:
+            print(f"{num}: NO")
+
+# Sample test
+numbers = [0, 1, 2, 3, 4, 8, 16, -2, -8, 1024, 218, -1]
+check_numbers(numbers)
